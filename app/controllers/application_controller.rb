9378e0dc6d816b727b40c3e63620f29ba56f1502
Alan Maik <alanmaikm@gmail.com>
2019-01-25 22:00:01 -0300

    Model user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@
 
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+
+  private
+
+  def not_found(exception)
+    render status: :not_found, json: {
+      id: 'not_found',
+      message: exception.message
+    }
+  end
+
+  def unprocessable_entity(exception)
+    render status: :unprocessable_entity, json: {
+      id: 'unprocessable_entity',
+      message: exception.message
+    }
+  end
 end
