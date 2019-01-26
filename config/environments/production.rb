bf223c778a8f4c30d8d814a18b5801070d4b49f3
Alan Maik <alanmaikm@gmail.com>
2019-01-25 23:13:20 -0300

    change application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  # Settings specified here will take precedence
-  # over those in
+  # Settings specified here will take precedence over those in
   # config/application.rb.
 
   # Code is not reloaded between requests.
@@ -27,16 +26,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # `config.assets.precompile` and `config.assets.version` have moved to
-  # config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -69,10 +58,10 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job
-  # (and separate queues per environment)
+  # Use a real queuing backend for Active Job (and separate queues per
+  # environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "gcer-api_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "teledipity-api_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
@@ -93,8 +82,8 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new
-  # (Syslog::Logger.new 'app-name')
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new
+  # 'app-name')
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
