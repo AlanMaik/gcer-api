19820d9da7ae34456ecbb6da7c5997a2bf311787
Alan Maik <alanmaikm@gmail.com>
2019-01-28 00:43:43 -0300

    improve coverage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,4 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token
-
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
-
-  private
-
-  def not_found(exception)
-    render status: :not_found, json: {
-      id: 'not_found',
-      message: exception.message
-    }
-  end
-
-  def unprocessable_entity(exception)
-    render status: :unprocessable_entity, json: {
-      id: 'unprocessable_entity',
-      message: exception.message
-    }
-  end
 end
