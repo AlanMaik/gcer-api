d81e6e30913314c4f55583b78c912327efde2708
Alan Maik <alanmaikm@gmail.com>
2019-02-04 21:39:28 -0300

    Create Endpoint users create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
     render json: user
     response.set_header('X-Total-Count', user.count)
   end
+
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  def create
+    user = User.create!(
+      name: params[:name],
+      email: params[:email],
+      last_name: params[:last_name],
+      kind: params[:kind],
+      cpf: params[:cpf],
+      phone: params[:phone],
+      specialty: params[:specialty],
+      password: params[:password]
+    )
+    render status: :created, json: user
+  end
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 end
---
+++ spec/request/users/create_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'GET /users', type: :request do
+  subject { response.body }
+
+  let(:params) do
+    {
+      name: 'foobar',
+      email: 'foobar@bar.com',
+      last_name: 'baz',
+      kind: 'gerente',
+      cpf: '12345678901',
+      phone: '123456789',
+      specialty: 'podologa',
+      password: '12345678'
+    }.to_json
+  end
+
+  before do
+    post '/users', params: params
+  end
+
+  it { expect(response).to have_http_status(:created) }
+  it { expect(User.count).to eq(2) }
+end
