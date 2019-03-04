7d4f38400988c082ec966c78cd72f51c821dfbd3
Alan Maik <alanmaikm@gmail.com>
2019-03-03 23:04:46 -0300

    Configuring Authentication

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ClientsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     client = Client.all
     render json: client
